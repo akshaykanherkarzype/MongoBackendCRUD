363016870ec0069f2c4f2814988c25ebc2b93d34
akshaykanherkarzype <akshay.kanherkar@getzype.com>
2024-04-04 20:10:49 +0530

    added dalvi

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const NotesController = require("./Controllers/notes.route");
 const Authentication = require("./Middlewares/Authentication");
 
 app.get("/", (req, res) => {
-  res.send("This is Homepage, from Akshay, wada");
+  res.send("This is Homepage, from Akshay, chyaaila ghoda");
 });
 
 app.use("/user", userController);
