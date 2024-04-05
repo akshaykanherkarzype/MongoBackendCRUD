1ad0bf538a4d5a3f7e921ae8b73bbc463da9ca0d
akshaykanherkarzype <akshay.kanherkar@getzype.com>
2024-04-05 16:52:28 +0530

    don

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const NotesController = require("./Controllers/notes.route");
 const Authentication = require("./Middlewares/Authentication");
 
 app.get("/", (req, res) => {
-  res.send("This is Homepage, from Akshay, Hii");
+  res.send("This is Homepage, from Akshay, Harshal Don");
 });
 
 app.use("/user", userController);
